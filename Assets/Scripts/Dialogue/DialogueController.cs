7971ffeb3f6c28ec46ce555e1a8c4a912d7cf8e8
John Mai <johnhuymai@gmail.com>
2022-11-12 00:04:25 -0600

    Dialogue controller working
    Cutscene scene is missing backgrounds and final dialogue.

--- Assets/Scripts/Dialogue/DialogueController.cs
+++ Assets/Scripts/Dialogue/DialogueController.cs
@@ -1,36 +1,71 @@
+using Newtonsoft.Json.Bson;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DialogueController : MonoBehaviour
 {
     [SerializeField] DialogueManager _dialogueManager;
-    [SerializeField] DialogueData[] _dialogueArray;
-    private int _dialogueIndex;
+    [SerializeField] private DialogueData[] _dialogueArray = null;
+
     private DialogueData _activeDialogueData;
-    private bool _dialogueInitialized;
+    private int _dialogueIndex;
+    private bool _endCutscene;
 
-
-    private void Awake()
+    private void Start()
     {
         _dialogueIndex = 0;
-        _activeDialogueData = _dialogueArray[_dialogueIndex];
+        _endCutscene = false;
+
+        // Assign active dialogue and pass to manager if valid
+        if (_dialogueArray[_dialogueIndex] != null)
+        {
+            _activeDialogueData = _dialogueArray[_dialogueIndex];
+            _dialogueManager.StartDialogue(_activeDialogueData);
+        }
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && (!_endCutscene))
         {
             if (!_dialogueManager.DialogueStarted)
             {
-                _dialogueManager.StartDialogue(_activeDialogueData);
+                // Load in the next DialogueData object in DialogueArray
+                IndexDialogue();
             }
 
             else
             {
                 _dialogueManager.DisplayNextSentence();
             }
-            
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Return) && (_endCutscene))
+        {
+            // Reconsider loading a specific scene by name / string
+            int currentActiveSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+            SceneManager.LoadScene(currentActiveSceneIndex + 1);
+        }
+    }
+
+    private void IndexDialogue()
+    {
+        _dialogueIndex++;
+
+        if (_dialogueIndex < _dialogueArray.Length)
+        {
+            _activeDialogueData = _dialogueArray[_dialogueIndex];
+            _dialogueManager.StartDialogue(_activeDialogueData);
+            return;
+        }
+
+        else
+        {
+            _dialogueManager.EndCutscene();
+            _endCutscene = true;
         }
     }
 }
--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -5,52 +5,62 @@ using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] private TextMeshProUGUI _characterNameHolder;
     [SerializeField] private TextMeshProUGUI _dialogueHolder;
+    [SerializeField] private TextMeshProUGUI _continueDialogue;
 
-    private int _dialogueIndex = 0;
     private bool _dialogueStarted = false;
-    /*private bool _dialogueEnded = false;
-    public bool DialogueEnded => _dialogueEnded;*/
+    // Queue for FIFO
     private Queue<string> sentences;
 
     public bool DialogueStarted => _dialogueStarted;
 
-    //Initialization
-    private void Start()
+    private void Awake()
     {
+        _continueDialogue.enabled = false;
         sentences = new Queue<string>();
     }
 
+    // Load in new CharacterName and start new queue
     public void StartDialogue(DialogueData dialogueData)
     {
         _dialogueStarted = true;
+
+        // Load in character name into CharacterName UI
         _characterNameHolder.text = dialogueData.CharacterName;
 
         sentences.Clear();
 
+        // Add all string from DialogueData array to sentences queue
         foreach (string _dialogue in dialogueData.Dialogues){
             sentences.Enqueue(_dialogue);
         }
 
+        // Automatically load in sentence into DialogueBox UI
         DisplayNextSentence();
     }
 
     public void DisplayNextSentence()
     {
+        // Remove a string from the sentences queue and load it into the DialogueBox UI
+        string sentence = sentences.Dequeue();
+        _dialogueHolder.text = sentence;
+
         if (sentences.Count == 0)
         {
             EndDialogue();
             return;
         }
-
-        string sentence = sentences.Dequeue();
-        _dialogueHolder.text = sentence;
     }
 
     public void EndDialogue()
     {
-        _dialogueStarted = true;
-        //_dialogueEnded = true;
+        _dialogueStarted = false;
+    }
+
+    public void EndCutscene()
+    {
+        _continueDialogue.enabled = true;
     }
 }
